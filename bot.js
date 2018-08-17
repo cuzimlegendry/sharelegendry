53bc63a1c046d6361613ba725583dd6a117a6437
cuzimlegendry <42479098+cuzimlegendry@users.noreply.github.com>
2018-08-17 21:39:07 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,63 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.login('NDc0Mjc2MTkxMTE3NzcwNzUz.Dlg0zQ.107DeLWXNrfIQurQsrOyJmlRMWY');
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+    console.log(`in ${client.guilds.size} servers `)
+    console.log(`${client.users.size} Users`)
+});
+client.on('ready', () => {
+    client.user.setActivity("You",{type: 'WATCHING'})
+
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**
+=========================[ ${member} ]=========================
+
+
+                                                     اقدم لك افضل سيرفر ديسكورد و بيه بوتات نااااار :fire:
+
+                                                  تعال و جرب بـ نفسك يا قلبي سيرفر مرا خرافي :heart: :fire:
+
+                                        لا تنسي تنشر السيرفر يا قلبي عشان بيه جيف اواي كتير بالطريق :heart: :fire:
+
+                                                                                     السيرفر
+
+
+=======================[ https://discord.gg/K5zBSfH ]=========================
+**`)
+}).catch(console.error)
+})
+
+const discord = require('discord.js');
+const a7med = new Discord.Client();
+
+client.on("guildMemberRemove", member => {
+  member.createDM().then(function (channel) {
+  return channel.send(`
+**
+=========================[ ${member} ]=========================
+
+
+                                                     اقدم لك افضل سيرفر ديسكورد و بيه بوتات نااااار :fire:
+
+                                                  تعال و جرب بـ نفسك يا قلبي سيرفر مرا خرافي :heart: :fire:
+
+                                        لا تنسي تنشر السيرفر يا قلبي عشان بيه جيف اواي كتير بالطريق :heart: :fire:
+
+                                                                                     السيرفر
+
+
+=======================[ https://discord.gg/K5zBSfH ]=========================
+**`)
+}).catch(console.error)
+});
