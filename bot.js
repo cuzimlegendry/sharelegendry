878bc968ca6093d41ca0c87334a6201a141112b8
cuzimlegendry <42479098+cuzimlegendry@users.noreply.github.com>
2018-08-19 05:22:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -62,7 +62,7 @@ client.on("guildMemberRemove", member => {
 
 client.on('message', message => {
   if(!message.channel.guild) return;
-if(message.content.startsWith('$bc')) {
+if(message.content.startsWith('.bc')) {
  if (message.author.id !== '474276191117770753') return message.reply('**Not For You !**')
  if(!message.author.id === '474276191117770753') return;
 let args = message.content.split(" ").join(" ").slice(2 + prefix.length);
@@ -73,11 +73,39 @@ msg.react('✅')
 .then(() => msg.react('❌'))
 .then(() =>msg.react('✅'))
 
+let reaction1Filter = (reaction, user) => reaction.emoji.name === '✅' && user.id === message.author.id;
+let reaction2Filter = (reaction, user) => reaction.emoji.name === '❌' && user.id === message.author.id;
+let reaction1 = msg.createReactionCollector(reaction1Filter, { time: 12000 });
+let reaction2 = msg.createReactionCollector(reaction2Filter, { time: 12000 });
+reaction1.on("collect", r => {
+message.channel.send(`☑ | Done ... The Broadcast Message Has Been Sent For ${message.guild.members.size} Members`).then(m => m.delete(5000));
+message.guild.members.forEach(m => {
+var bc = new
+Discord.RichEmbed()
+.setColor('RANDOM')
+.setTitle('Broadcast')
+.addField('Server', message.guild.name)
+.addField('Sender', message.author.username)
+.addField('Message', args)
+.setThumbnail(message.author.avatarURL)
+.setFooter(copy, client.user.avatarURL);
+m.send({ embed: bc })
+msg.delete();
+})
+})
+reaction2.on("collect", r => {
+message.channel.send(`**Broadcast Canceled.**`).then(m => m.delete(5000));
+msg.delete();
+})
+})
+}
+});
+
 client.on('message', message => {
             if(!message.channel.guild) return;
 let args = message.content.split(' ').slice(1).join(' ');
 if (message.content.startsWith('.bcall')){
- if (message.author.id !== '474276191117770753') return message.reply('** هذا الأمر قفط لصاحب البوت و شكراًً **')
+ if (message.author.id !== '474276191117770753') return message.reply('**Not For You !**')
  if(!message.author.id === '474276191117770753') return;
 message.channel.sendMessage('جار ارسال الرسالة |✅')
 client.users.forEach(m =>{
